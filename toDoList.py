02181597685a8299734da28bb35ed79287ab6cac
theG-123 <153643437+theG-123@users.noreply.github.com>
2024-04-22 18:24:19 +0000

    Planning the app

---
+++ toDoList.py
@@ -0,0 +1,32 @@
+# Description: Build a to-do list manager application that allows users to create, update, and delete tasks. The application should
+# support features like categorizing tasks, setting deadlines, marking tasks as complete, and searching/filtering tasks.
+# Skills practiced: Object-oriented programming (OOP), file I/O, data manipulation, user input validation, basic CRUD operations.
+
+# Bonus features:
+# Reminders: Implement reminders for upcoming deadlines or tasks.
+# User authentication: Add user authentication to allow multiple users to have their own to-do lists.
+# Priority levels: Allow users to assign priority levels to tasks and sort/filter tasks based on priority.
+# These projects offer a good balance of complexity and learning opportunities, allowing you to apply various Python concepts and 
+# libraries while building useful applications. Feel free to start with one project and gradually move to the other as you gain
+# confidence and proficiency in Python programming.
+
+
+def categorize_tasks():
+    print ('This is the function that will let users sort their different tasks into categories')
+    #For example: Sports, School, Coding, Homework/AfterSchool
+
+def create_task():
+    print ('Function to create tasks')
+    #In this function, include a feature that asks how important is each task. There will be three levels:
+    #Urgent: Tasks that need immediate attention or must be done as soon as possible.
+    #Priority: Tasks that are important but not urgent, requiring attention in the near future.
+    #Casual: Tasks that are less urgent or can be done at leisure when there is ample time available.
+
+def remove_task():
+    #Function to remove tasks
+
+def modify_tasks():
+    #Function to modify tasks
+
+def filter_tasks():
+    #function to filter tasks by search
