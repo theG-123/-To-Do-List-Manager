a20a7c117a586f37d4dbf0fc6bdcd2f6f1d5e780
theG-123 <153643437+theG-123@users.noreply.github.com>
2024-04-24 19:14:08 +0000

    User creation/login for to do list almost done

--- toDoList.py
+++ toDoList.py
@@ -10,6 +10,7 @@
 # libraries while building useful applications. Feel free to start with one project and gradually move to the other as you gain
 # confidence and proficiency in Python programming.
 
+import os
 
 def select_user():
     print('Welcome to your to do list manager! Are you a new or existing user?')
@@ -18,17 +19,40 @@ def select_user():
         print('\nInvalid selection, please try again.\n')
         return select_user()
 
+    #Print a confirmation message for options 1 & 2 
     confirmation_message = {
-        '1': 'You have selected new user. Is this correct? (y/n)',
-        '2': 'You have selected existing user. Is this correct? (y/n)'
+        '1': 'You have selected new user. Do you want to go back? (y/n)',
+        '2': 'You have selected existing user. Do you want to go back? (y/n)'
     }
+
     confirmation = input(confirmation_message[user_selection]).lower()
     if confirmation == 'y':
-        return 'ok'
-    else:
-        print ('\nAs you said no, we will take you back to the homepage\n')
+        print ('\nTaking you back...\n')
         return select_user()
-print(select_user())
+    else:
+        print('Lets continue!')
+
+    if user_selection == '1': #Create a new user
+        while True:
+            create_user = input ('Create username: ')
+            if os.path.exists(create_user + '_to-do-list'):
+                print('That username is already taken. Please choose a different username.')
+                continue
+            else:
+                with open(create_user + '_to-do-list', 'w') as open_to_do_list:
+                    print('User & to do list created successfully!')
+                break
+    elif user_selection == '2': #Log into the existing user
+        while True:
+            existing_user = input ('What user do you wish to log into? ')
+            if not os.path.exists(existing_user + '_to-do-list'):
+                print('That user does not exist. Please try again.')
+                continue
+            else:
+                with open(existing_user + '_to-do-list') as open_to_do_list:
+                    break
+
+select_user()
 
 def categorize_tasks():
     print ('This is the function that will let users sort their different tasks into categories')
